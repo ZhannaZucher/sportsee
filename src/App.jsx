576708db9eb12a31cce490107101f94184504a3e
ZhannaZucher <jzucher@gmail.com>
2023-06-26 17:15:26 +0200

    add : vertical menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom"
 import Profile from "./pages/Profile/Profile"
 import Header from "./components/Header/Header"
+import VerticalNav from "./components/VerticalNav/VerticalNav"
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
+      <VerticalNav />
       <Routes>
         <Route path="/:id" element={<Profile />} />
       </Routes>
