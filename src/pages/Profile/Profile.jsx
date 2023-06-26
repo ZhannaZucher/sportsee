bcd5b3a6a3c9356789579a22ac1bb8481d718041
ZhannaZucher <jzucher@gmail.com>
2023-06-26 19:21:55 +0200

    add: mockedData

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,19 +1,24 @@
+import mockData from "../../data/data"
+import { useParams } from "react-router-dom"
 import Card from "../../components/Card/Card"
 import energy from "../../assets/calories-icon.png"
 import chicken from "../../assets/protein-icon.png"
 import apple from "../../assets/carbs-icon.png"
 import burger from "../../assets/fat-icon.png"
-import VerticalNav from "../../components/VerticalNav/VerticalNav"
 import "./Profile.css"
 
 export default function Profile() {
-  return (
+  const userMainData = mockData.USER_MAIN_DATA
+  const { id } = useParams()
+  const queryId = userMainData.find((user) => user.id === parseInt(id))
+  console.log(queryId)
+
+  return queryId ? (
     <main>
-      <VerticalNav />
       <div className="content">
         <div className="greeting">
           <p className="greeting__title">
-            Bonjour <span>Thomas</span>
+            Bonjour <span>{queryId.userInfos.firstName}</span>
           </p>
           <p className="greeting__subtitle">
             Félicitations ! Vous avez explosé vos objectifs hier 👏
@@ -25,15 +30,34 @@ export default function Profile() {
             <Card
               icon={energy}
               alt="energy"
-              value="1,930kCal"
+              value={`${queryId.keyData.calorieCount}kCal`}
               type="Calories"
             />
-            <Card icon={chicken} alt="chicken" value="155g" type="Proteines" />
-            <Card icon={apple} alt="apple" value="290g" type="Glucides" />
-            <Card icon={burger} alt="burger" value="90g" type="Lipides" />
+            <Card
+              icon={chicken}
+              alt="chicken"
+              value={`${queryId.keyData.proteinCount}g`}
+              type="Proteines"
+            />
+            <Card
+              icon={apple}
+              alt="apple"
+              value={`${queryId.keyData.carbohydrateCount}g`}
+              type="Glucides"
+            />
+            <Card
+              icon={burger}
+              alt="burger"
+              value={`${queryId.keyData.lipidCount}g`}
+              type="Lipides"
+            />
           </aside>
         </div>
       </div>
     </main>
+  ) : (
+    <div className="error">
+      <p> Oups ! Cet utilisateur n'existe pas.</p>
+    </div>
   )
 }
