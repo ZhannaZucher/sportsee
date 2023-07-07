d9e7fd6a2f4be78bc527a5f4c439ac8378c61f0c
ZhannaZucher <jzucher@gmail.com>
2023-07-07 11:14:18 +0200

    customized tooltip with ref area

---
+++ src/components/CustomTooltipSessions/CustomTooltipSessions.jsx
@@ -0,0 +1,13 @@
+import PropTypes from "prop-types"
+import "./CustomTooltipSessions.css"
+
+export default function CustomTooltipSessions({ active, payload }) {
+  return active && payload && payload.length ? (
+    <p className="sessions__tooltip-label">{`${payload[0].value} min`}</p>
+  ) : null
+}
+
+CustomTooltipSessions.propTypes = {
+  active: PropTypes.bool,
+  payload: PropTypes.array,
+}
