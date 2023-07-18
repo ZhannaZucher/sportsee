67cf24aca4dd33eeb86d640f2276b245504ac29e
ZhannaZucher <jzucher@gmail.com>
2023-07-18 18:12:56 +0200

    add: navigation to home page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,11 +5,11 @@ import "./Header.css"
 export default function Header() {
   return (
     <header className="header">
-      <Link className="header__logo" to="#">
+      <Link className="header__logo" to="/">
         <img src={logo} alt="logo" />
       </Link>
       <nav className="header__nav">
-        <NavLink to="#">Accueil</NavLink>
+        <NavLink to="/">Accueil</NavLink>
         <NavLink to="#">Profil</NavLink>
         <NavLink to="#">Réglage</NavLink>
         <NavLink to="#">Communauté</NavLink>
